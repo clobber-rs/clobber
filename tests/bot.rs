8294ca4041eb7605fccb1025d8675daa33a5709b
Emelie <em@nao.sh>
2021-09-13 11:04:09 +0200

    should work now

--- tests/bot.rs
+++ tests/bot.rs
@@ -1,8 +1,15 @@
+use std::convert::TryFrom;
+
+use matrix_sdk::ruma::UserId;
+
 mod common;
 
 #[tokio::test]
 async fn test() -> anyhow::Result<()> {
     let client = common::get_client().await;
-    println!("{:?}", client.whoami().await?);
+    assert_eq!(
+        client.whoami().await?.user_id,
+        UserId::try_from("@clobber:localhost:6167").unwrap()
+    );
     Ok(())
 }
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -4,10 +4,14 @@ use matrix_sdk::reqwest::Url;
 use matrix_sdk::ruma::api::client::r0::account::register::Request as RegistrationRequest;
 use matrix_sdk::ruma::api::client::r0::uiaa::AuthData;
 use matrix_sdk::ruma::assign;
-use matrix_sdk::{Client, SyncSettings};
+use matrix_sdk::{Client, Session, SyncSettings};
 use tokio::sync::OnceCell;
 static CLIENT: OnceCell<Client> = OnceCell::const_new();
 
+/// Sets up a  `matrix_sdk::Client` for use within integration tests. By making use of
+/// `tokio::sync::OnceCell`, the first function call will set up a client and register and log in
+/// to the CI embedded conduit instance, returning the client. Any subsequent calls will simply return the
+/// client, already set up and ready to use.
 pub async fn get_client() -> Client {
     CLIENT
         .get_or_init(|| async {
@@ -21,10 +25,10 @@ pub async fn get_client() -> Client {
             let uiaa = match client.register(request.clone()).await {
                 Err(e) => match e.uiaa_response().cloned() {
                     Some(uiaa) => uiaa,
-                    None => panic!("Missing UIAA response"),
+                    None => panic!("Missing UIAA response 1"),
                 },
                 Ok(_) => {
-                    panic!("Missing UIAA response")
+                    panic!("Missing UIAA response 2")
                 }
             };
             // Get the first step in the authentication flow (we're ignoring the rest)
@@ -32,11 +36,18 @@ pub async fn get_client() -> Client {
             let kind = stages.and_then(|flow| flow.stages.get(0)).cloned();
             // Set authentication data, fallback to password type
             request.auth = Some(AuthData::DirectRequest {
-                kind: kind.as_deref().unwrap_or("m.login.password"),
+                kind: kind.as_deref().unwrap_or("m.login.dummy"),
                 session: uiaa.session.as_deref(),
                 auth_parameters: Default::default(),
             });
-            client.register(request).await.unwrap();
+            let response = client.register(request).await.unwrap();
+            let session = Session {
+                access_token: response.access_token.unwrap(),
+                user_id: response.user_id,
+                device_id: response.device_id.unwrap(),
+            };
+            // Not entirely sure why this is necessary but it works ¯\_(ツ)_/¯
+            client.restore_login(session).await.unwrap();
             client.sync_once(SyncSettings::new()).await.unwrap();
             client
         })
