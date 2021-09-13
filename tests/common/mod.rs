02792ad129e0171d677ddd4c1fa073c272bf824d
Emelie <em@nao.sh>
2021-09-13 09:23:59 +0200

    handle registration properly

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -12,12 +12,32 @@ pub async fn get_client() -> Client {
     CLIENT
         .get_or_init(|| async {
             let client = Client::new(Url::try_from("http://localhost:6167").unwrap()).unwrap();
-            let request = assign!(RegistrationRequest::new(), {
+            let mut request = assign!(RegistrationRequest::new(), {
                 username: Some("clobber"),
                 password: Some("foobar"),
-                auth: Some(AuthData::direct_request("m.login.password"))
+                inhibit_login: false
             });
-            client.register(request).await.unwrap();
+            // Get UIAA session key
+            let uiaa = match client.register(request.clone()).await {
+                Err(e) => match e.uiaa_response().cloned() {
+                    Some(uiaa) => uiaa,
+                    None => return Err(anyhow::anyhow!("Missing UIAA response")),
+                },
+                Ok(_) => {
+                    return Err(anyhow::anyhow!("Missing UIAA response"));
+                }
+            };
+            // Get the first step in the authentication flow (we're ignoring the rest)
+            let stages = uiaa.flows.get(0);
+            let kind = stages.and_then(|flow| flow.stages.get(0)).cloned();
+            // Set authentication data, fallback to password type
+            request.auth = Some(AuthData::DirectRequest {
+                kind: kind.as_deref().unwrap_or("m.login.password"),
+                session: uiaa.session.as_deref(),
+                auth_parameters: Default::default(),
+            });
+            let response = client.register(request).await?;
+            client.sync_once(SyncSettings::new()).await?;
             client
         })
         .await
