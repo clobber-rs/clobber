3b9be07234887dd0bcc9f9895c0201abb72545af
Emelie <em@nao.sh>
2021-09-17 14:11:55 +0200

    wip: Add integration test for mute command

--- tests/bot.rs
+++ tests/bot.rs
@@ -2,9 +2,20 @@
 // Copyright (C) 2020 Emelie Graven <em@nao.sh>
 // Licensed under the EUPL
 
-use std::convert::TryFrom;
+use std::{convert::TryFrom, time::Duration};
 
-use matrix_sdk::ruma::UserId;
+use matrix_sdk::{
+    room::Room,
+    ruma::{
+        api::client::r0::room::create_room::Request,
+        events::{room::message::MessageEventContent, AnyMessageEventContent},
+        user_id, UserId,
+    },
+    SyncSettings,
+};
+use tokio::time::sleep;
+
+use anyhow::anyhow;
 
 mod common;
 
@@ -15,5 +26,61 @@ async fn test() -> anyhow::Result<()> {
         client.whoami().await?.user_id,
         UserId::try_from("@clobber:localhost:6167").unwrap()
     );
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_mute() -> anyhow::Result<()> {
+    let client = common::get_client().await;
+    let client_clone = client.clone();
+    let sync = tokio::spawn(async move {
+        let client = client_clone;
+        client
+            .sync(SyncSettings::default().token(client.sync_token().await.unwrap()))
+            .await;
+    });
+    let client_target = common::get_client_target().await;
+    let room = client.create_room(Request::new()).await?;
+    client.join_room_by_id(&room.room_id).await?;
+    let mut delay = 2;
+    let room = loop {
+        if let Some(room) = client.get_joined_room(&room.room_id) {
+            break room;
+        }
+        sleep(Duration::from_secs(delay)).await;
+        delay *= 2;
+
+        if delay > 3600 {
+            anyhow!("Joining room {} timed out", &room.room_id);
+            panic!();
+        }
+    };
+    room.invite_user_by_id(&user_id!("@target:localhost"))
+        .await?;
+    let target_room = client_target.get_room(room.room_id()).unwrap();
+    if let Room::Invited(target_room) = target_room {
+        let mut delay = 2;
+        while target_room.accept_invitation().await.is_err() {
+            sleep(Duration::from_secs(delay)).await;
+            delay *= 2;
+
+            if delay > 3600 {
+                anyhow!("Joining room {} timed out", target_room.room_id());
+                break;
+            }
+        }
+    }
+
+    let content = AnyMessageEventContent::RoomMessage(MessageEventContent::text_plain(
+        "?mute @target:localhost",
+    ));
+    room.send(content, None).await.unwrap();
+    let pl = room
+        .get_member(&client_target.user_id().await.unwrap())
+        .await?
+        .unwrap()
+        .power_level();
+    assert_eq!(pl, -1);
+    sync.abort();
     Ok(())
 }
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -4,13 +4,20 @@
 
 use std::convert::TryFrom;
 
+use clobber::bot;
+use clobber::config::{Bot, Config, Homeserver};
 use matrix_sdk::reqwest::Url;
+use matrix_sdk::room::Room;
 use matrix_sdk::ruma::api::client::r0::account::register::Request as RegistrationRequest;
 use matrix_sdk::ruma::api::client::r0::uiaa::{AuthData, Dummy};
-use matrix_sdk::ruma::assign;
+use matrix_sdk::ruma::events::room::member::MemberEventContent;
+use matrix_sdk::ruma::events::room::message::MessageEventContent;
+use matrix_sdk::ruma::events::{StrippedStateEvent, SyncMessageEvent};
+use matrix_sdk::ruma::{assign, UserId};
 use matrix_sdk::{Client, Session, SyncSettings};
 use tokio::sync::OnceCell;
 static CLIENT: OnceCell<Client> = OnceCell::const_new();
+static CLIENT_TARGET: OnceCell<Client> = OnceCell::const_new();
 
 /// Sets up a  `matrix_sdk::Client` for use within integration tests. By making use of
 /// `tokio::sync::OnceCell`, the first function call will set up a client and register and log in
@@ -19,38 +26,83 @@ static CLIENT: OnceCell<Client> = OnceCell::const_new();
 pub async fn get_client() -> Client {
     CLIENT
         .get_or_init(|| async {
-            let client = Client::new(Url::try_from("http://localhost:6167").unwrap()).unwrap();
-            let mut request = assign!(RegistrationRequest::new(), {
-                username: Some("clobber"),
-                password: Some("foobar"),
-                inhibit_login: false
-            });
-            // Get UIAA session key
-            let uiaa = match client.register(request.clone()).await {
-                Err(e) => match e.uiaa_response().cloned() {
-                    Some(uiaa) => uiaa,
-                    None => panic!("Missing UIAA response 1"),
-                },
-                Ok(_) => {
-                    panic!("Missing UIAA response 2")
-                }
-            };
-            // Set authentication data, m.login.dummy
-            let dummy = assign!(Dummy::new(), {
-                session: uiaa.session.as_deref()
-            });
-            request.auth = Some(AuthData::Dummy(dummy));
-            let response = client.register(request).await.unwrap();
-            let session = Session {
-                access_token: response.access_token.unwrap(),
-                user_id: response.user_id,
-                device_id: response.device_id.unwrap(),
-            };
-            // Not entirely sure why this is necessary but it works ¯\_(ツ)_/¯
-            client.restore_login(session).await.unwrap();
-            client.sync_once(SyncSettings::new()).await.unwrap();
-            client
+            let _ = tracing::subscriber::set_global_default(
+                tracing_subscriber::fmt().pretty().finish(),
+            );
+            init_client("clobber").await
         })
         .await
         .clone()
+}
+
+/// Sets up another `matrix_sdk::Client` like `get_client()` to be used as a moderation target.
+pub async fn get_client_target() -> Client {
+    CLIENT_TARGET
+        .get_or_init(|| async { init_client("target").await })
+        .await
+        .clone()
+}
+
+async fn init_client(username: &str) -> Client {
+    let client = Client::new(Url::try_from("http://localhost:6167").unwrap()).unwrap();
+    let mut request = assign!(RegistrationRequest::new(), {
+        username: Some(username),
+        password: Some("password"),
+        inhibit_login: false
+    });
+    // Get UIAA session key
+    let uiaa = match client.register(request.clone()).await {
+        Err(e) => match e.uiaa_response().cloned() {
+            Some(uiaa) => uiaa,
+            None => panic!("Missing UIAA response 1"),
+        },
+        Ok(_) => {
+            panic!("Missing UIAA response 2")
+        }
+    };
+    // Set authentication data, m.login.dummy
+    let dummy = assign!(Dummy::new(), {
+        session: uiaa.session.as_deref()
+    });
+    request.auth = Some(AuthData::Dummy(dummy));
+    let response = client.register(request).await.unwrap();
+    let session = Session {
+        access_token: response.access_token.unwrap(),
+        user_id: response.user_id,
+        device_id: response.device_id.unwrap(),
+    };
+    // Not entirely sure why this is necessary but it works ¯\_(ツ)_/¯
+    client.restore_login(session).await.unwrap();
+    client.sync_once(SyncSettings::new()).await.unwrap();
+
+    let config = Config {
+        homeserver: Homeserver {
+            url: String::from("http://localhost:6167"),
+        },
+        bot: Bot {
+            command_prefix: String::from("?"),
+            allow_invites: vec![UserId::try_from("@clobber:localhost").unwrap()],
+        },
+    };
+
+    client.register_event_handler(bot::on_room_member).await;
+    client
+        .register_event_handler({
+            let config = config.clone();
+            move |ev: SyncMessageEvent<MessageEventContent>, room: Room, client: Client| {
+                let config = config.clone();
+                async move { bot::on_room_message(ev, room, client, config).await }
+            }
+        })
+        .await;
+    client
+        .register_event_handler({
+            let config = config.clone();
+            move |ev: StrippedStateEvent<MemberEventContent>, room: Room, client: Client| {
+                let config = config.clone();
+                async move { bot::on_stripped_state_member(ev, room, client, config).await }
+            }
+        })
+        .await;
+    client
 }
